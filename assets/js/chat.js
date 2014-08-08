a4688afa189ae0f27f04d0b8daa23b11e42eac6d
Yann Gouffon <yago@yago-art.ch>
2014-08-08 13:33:48 +0200

    fix chat field

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -10,8 +10,8 @@
       if(index>=data.length-1){
         clearInterval(T);
         $('#cursor').hide();
-        $('#fchat').show();
-        $('#fchat-message').triggerHandler( "focus" );
+        $('head').append('<style>#fchat {display:block !important;}</style>');
+        $('#fchat-message').focus();
       }
       _span = '<span>'+data[index]+'</span>';
       $('#cursor').before(_span);
